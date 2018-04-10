c89db1bc0930138793329c66445927540713b10c
RasskazovIlya <36595894+thefogamer@users.noreply.github.com>
2018-04-10 19:05:48 +0300

    АЦП

--- 5/5.c
+++ 5/5.c
@@ -1,5 +1,12 @@
 #include "stm32f10x.h"
 
+#define PRESCALER_PWM 1 //предделитель таймера
+#define PERIOD_PWM 960 //период ШИМ
+#define PULSE_PWM 0 //скважность ШИМ
+
+GPIO_InitTypeDef GPIO_LED; 
+TIM_TimeBaseInitTypeDef Tim;
+TIM_OCInitTypeDef TimPWM;
 ADC_InitTypeDef ADC_1;
 
 uint32_t delay_count = 0;
@@ -39,9 +46,39 @@ void initTACT(void)
 
 void initALL(void)
 {
-	//включаем тактирование АЦП1
+	//включаем тактирование АЦП1, порта входа-выхода С и таймера 3
 	RCC_ADCCLKConfig(RCC_PCLK2_Div2);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+	
+	//включаем 8 ножку на GPIOC в альтернативном режиме
+	GPIO_StructInit(&GPIO_LED);
+	GPIO_LED.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_LED.GPIO_Pin = GPIO_Pin_8;
+	GPIO_LED.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_LED);
+	
+	//ремапим ножку, чтобы канал ШИМ был на 8 ножке
+	GPIO_PinRemapConfig(GPIO_FullRemap_TIM3, ENABLE);
+	
+	//настраиваем таймер: считает вверх, предделитель и период заданы в начале
+	TIM_TimeBaseStructInit(&Tim);
+	Tim.TIM_ClockDivision = TIM_CKD_DIV1;
+	Tim.TIM_CounterMode = TIM_CounterMode_Up;
+	Tim.TIM_Prescaler = PRESCALER_PWM - 1;
+	Tim.TIM_Period = PERIOD_PWM;
+	TIM_TimeBaseInit(TIM3, &Tim);
+	
+	//настраиваем генерацию ШИМ на таймере
+	TIM_OCStructInit(&TimPWM);
+	TimPWM.TIM_Pulse = PULSE_PWM;
+	TimPWM.TIM_OCMode = TIM_OCMode_PWM1;
+	TimPWM.TIM_OutputState = TIM_OutputState_Enable;
+	TimPWM.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC3Init(TIM3, &TimPWM);
+	
+	//включаем таймер
+	TIM_Cmd(TIM3, ENABLE);
 	
 	//настраиваем АЦП1
 	ADC_1.ADC_ContinuousConvMode = ENABLE; //сигнал измеряем постоянно
@@ -59,7 +96,7 @@ void initALL(void)
 	while(ADC_GetCalibrationStatus(ADC1)); //ждем, пока калибровка пройдет
 	ADC_TempSensorVrefintCmd(ENABLE);//включаем датчик температуры
 	
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_16, 1, ADC_SampleTime_28Cycles5); //настраиваем 16 канал (датчик температуры)
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor, 1, ADC_SampleTime_28Cycles5); //настраиваем 16 канал (датчик температуры)
 	ADC_SoftwareStartConvCmd(ADC1, ENABLE);//запускаем АЦП
 	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
 	
@@ -83,14 +120,23 @@ void SysTick_Handler(void)
 		delay_count--;
 }
 
+//обработчик прерываний АЦП1
 void ADC1_IRQHandler(void)
 {
-	float V25 = 1.41, Avg_Slope = 4.3*10e-3, Vref = 3.0, Vsense;
+	float V25 = 1.41, Avg_Slope = 4.3*10e-3, Vref = 3.3, Vsense;
 	
 	if(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC))
 	{
 		result = ADC_GetConversionValue(ADC1);
 		Vsense = result/4096.0*Vref;
-		temp = (V25 - Vsense)/Avg_Slope + 25.0;
+		temp = (V25 - Vsense)/Avg_Slope + 25.0; //считаем температуру по формуле
+		ADC_ClearITPendingBit(ADC1,ADC_IT_EOC);
 	}
+	
+	if (temp < 30 && temp > 15)
+		TIM_SetCompare3(TIM3, PERIOD_PWM/15*(uint16_t)(temp-15.0)); //изменяем яркость диода в зависимости от температуры
+	else if (temp > 30)
+		TIM_SetCompare3(TIM3, PERIOD_PWM); //если температура больше 30, то яркость максимальная
+	else if (temp < 15)
+		TIM_SetCompare3(TIM3, 0); //если температура меньше 15, то яркость 0, диод не горит
 }
